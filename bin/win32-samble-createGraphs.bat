3e718e468194544a71d46344af3709c26b0fbeea
tlornet <tlornet@teclib.com>
2016-09-12 14:43:09 +0200

    sample win32

--- bin/win32-samble-createGraphs.bat
+++ bin/win32-samble-createGraphs.bat
@@ -1,1 +1,1 @@
-perl -I../lib createHtmlGraphs.pl --logfile="C:\\Users\\tlornet\\Documents\\fia_branches\\fia_regular\\debug.log" --logfile="C:\\Users\\tlornet\\Documents\\fia_branches\\fia_regular_sigterm\\debug.log" --logfile="C:\\Users\\tlornet\\Documents\\fia_branches\\fia_thread_only\\debug.log" 
+perl -Ilib bin\createHtmlGraphs.pl --logfile="C:\\Users\\tlornet\\Documents\\fia_branches\\fia_regular\\debug.log" --logfile="C:\\Users\\tlornet\\Documents\\fia_branches\\fia_regular_sigterm\\debug.log" --logfile="C:\\Users\\tlornet\\Documents\\fia_branches\\fia_thread_only\\debug.log"
